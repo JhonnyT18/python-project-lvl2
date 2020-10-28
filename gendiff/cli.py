2351920c262c0ccf8ebf0f1d80bda2a47f7bfbb9
Evgenii T <chasej18@inbox.ru>
2020-10-29 00:30:47 +0300

    Add badge maintainability and update cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -8,4 +8,5 @@ def info_gendiff():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', metavar=('FORMAT'), help='set format of output')
     parser.parse_args()
