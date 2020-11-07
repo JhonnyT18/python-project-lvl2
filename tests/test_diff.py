83454623c0fc1f9b0681587cb6f6d9a304da915f
Evgenii T <chasej18@inbox.ru>
2020-11-07 12:18:51 +0300

    remove tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -4,9 +4,9 @@
 from gendiff.gendiff import generate_diff
 
 
-file = open('gendiff/tests/fixtures/expected.txt', 'r')
+file = open('tests/fixtures/expected.txt', 'r')
 result = file.read()
 
 
 def test_gendiff():
-    assert result == generate_diff('gendiff/tests/fixtures/file1.json', 'gendiff/tests/fixtures/file2.json')  # noqa: E501
+    assert result == generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')  # noqa: E501
