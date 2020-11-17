02fda2d3863ead47548dc6f4c6998beb7192265e
Evgenii T <chasej18@inbox.ru>
2020-11-18 00:42:00 +0300

    FixeD

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -2,7 +2,10 @@
 from gendiff.files_loader import get_data
 
 
-added, deleted, changed, changeless = ('added', 'deleted', 'changed', 'changeless')  # noqa: E501
+added = 'added'
+deleted = 'deleted'
+changed = 'changed'
+changeless = 'changeless'
 
 
 def make_diff(first_file, second_file):
@@ -23,21 +26,22 @@ def make_diff(first_file, second_file):
 
 
 def generate_diff(path_to_first, path_to_second):
-    first_file, second_file = (get_data(path_to_first), get_data(path_to_second))  # noqa: E501
+    first_file = get_data(path_to_first)
+    second_file = get_data(path_to_second)
     diff = make_diff(first_file, second_file)
+    indent = '   '
     keys = list(diff.keys())
     keys.sort()
     result = ''
-    for i in keys:
-        indent = '   '
-        condition, value = diff[i]
+    for key in keys:
+        condition, value = diff[key]
         if condition == 'changeless':
-            result += indent + '  ' + i + ': ' + str(value) + '\n'
+            result += indent + '  ' + key + ': ' + str(value) + '\n'
         elif condition == 'changed':
-            result += indent + '- ' + i + ': ' + str(value[0]) + '\n'
-            result += indent + '+ ' + i + ': ' + str(value[1]) + '\n'
-        elif condition == 'add':
-            result += indent + '+ ' + i + ': ' + str(value) + '\n'
+            result += indent + '- ' + key + ': ' + str(value[0]) + '\n'
+            result += indent + '+ ' + key + ': ' + str(value[1]) + '\n'
+        elif condition == 'added':
+            result += indent + '+ ' + key + ': ' + str(value) + '\n'
         elif condition == 'deleted':
-            result += indent + '- ' + i + ': ' + str(value) + '\n'
+            result += indent + '- ' + key + ': ' + str(value) + '\n'
     return '{}\n{}{}'.format('{', result, '}')
